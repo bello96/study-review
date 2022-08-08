8606d082d958a38d390581aa8eb353c3ea1cdc79
bello96 <bello96@163.com>
2022-08-08 22:49:31 +0800

    更新

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 // 使用for...of遍历数组如何获取索引
-// 可以利用entries()方法将数组转换为可迭代的对象
-let arr = ["你好", "在吗"];
+// 可以利用 entries() 方法将数组转换为可迭代的对象
+let arr = ["邓佳贝", "你好"];
 for (const [key, value] of arr.entries()) {
   console.log(key, value);
 }
