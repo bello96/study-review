3a444adc5ba2d5aee5e2881cbb83a3f4e2254a14
bello96 <bello96@163.com>
2022-08-13 00:20:34 +0800

    提交

--- js/utils.js
+++ js/utils.js
@@ -2,5 +2,12 @@
 // 可以利用 entries() 方法将数组转换为可迭代的对象
 let arr = ["邓佳贝", "你好", "呀"];
 for (const [key, value] of arr.entries()) {
-  console.log(key, value);
+  // console.log(key, value);
 }
+
+// 找出数组交集
+function bingji(a,b) {
+  return [...new Set([...a,...b])]
+}
+// let c = bingji([1,2,3],[2,0,3])
+// console.log(c)
