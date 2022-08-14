2958194e44573f2aff781da76118fc8880cddf88
jbdeng3 <jbdeng3@iflytek.com>
2022-08-14 12:05:24 +0800

    天天

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 // 使用for...of遍历数组如何获取索引
 // 可以利用 entries() 方法将数组转换为可迭代的对象
-let arr = ["邓佳贝", "你好", "呀"];
+let arr = ["佳贝", "你好", "呀"];
 for (const [key, value] of arr.entries()) {
   // console.log(key, value);
 }
